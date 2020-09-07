27d66e2d874888de85bc73a5359d7dd05cd21c4a
Albert <fltermare@gmail.com>
2020-09-07 20:24:12 +0800

    Add main-level domain

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -3,19 +3,68 @@ from celery import chain
 from celery_app import app
 from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch, helpers
+import string
+import random
+import tldextract
 
-# @app.task
-# def add(x, y):
-#     return x + y
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 
 @app.task(ignore_result=True)
-def push_crawl_data(data):
+def crawl_chain(urls):
+    # add_demo ->  mul_demo -> insert_db_demo
+    chain(crawl_url.s(urls), push_data.s())()
+
+
+@app.task(ignore_result=True)
+def crawl_url(urls):
+
+    # print(len(urls))
+    data = []
+    # crawling
+    for url in urls:
+
+        time.sleep(1)
+        content = "content_%s_%s" % (url, get_random_string(100))
+        meta = "meta_%s_%s" % (url, get_random_string(100))
+        extractor = tldextract.TLDExtract(cache_file='.tld_set')
+        ext = extractor(url)
+
+
+        # packing
+        crawl_time = datetime.now()
+        index = "index-" + crawl_time.strftime("%Y-%m-%d")
+        url_data = {
+            "_index": index,
+            "_source": {
+                "timestamp": crawl_time,
+                "url": url,
+                "content": content,
+                "metadata": meta,
+                "domain": ".".join([ext.domain, ext.suffix]),
+            }
+        }
+        data.append(url_data)
+
+
+    # time.sleep(1)
+    # es = Elasticsearch(['http://elastic:changeme@elasticsearch:9200'])
+    # helpers.bulk(es, data)
+    # return len(data)
+    return data
+
+
+@app.task(ignore_result=True)
+def push_data(data):
     time.sleep(1)
     es = Elasticsearch(['http://elastic:changeme@elasticsearch:9200'])
     # res = es.index(index=index, body=data)
     helpers.bulk(es, data)
-
     return len(data)
 
 
--- crawler.py
+++ crawler.py
@@ -3,12 +3,12 @@
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from typing import List
-from celery_app.tasks import push_crawl_data
+from celery_app.tasks import crawl_chain, crawl_url
 import concurrent.futures
 import time
-import string
+
 import sys
-import random
+
 # import urllib.request
 
 
@@ -25,45 +25,45 @@ def get_url_list(num: int) -> List[str]:
     return res
 
 
-def get_random_string(length):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
+# def get_random_string(length):
+#     letters = string.ascii_lowercase
+#     result_str = ''.join(random.choice(letters) for i in range(length))
+#     return result_str
 
 
-def get_content(urls, timeout):
-    # print(len(urls))
-    data = []
-    # crawling
-    for url in urls:
+# def get_content(urls, timeout):
+#     # print(len(urls))
+#     data = []
+#     # crawling
+#     for url in urls:
 
-        time.sleep(1)
-        content = "content_%s_%s" % (url, get_random_string(100))
-        meta = "meta_%s_%s" % (url, get_random_string(100))
+#         time.sleep(1)
+#         content = "content_%s_%s" % (url, get_random_string(100))
+#         meta = "meta_%s_%s" % (url, get_random_string(100))
 
-        # packing
-        crawl_time = datetime.now()
-        index = "index-" + crawl_time.strftime("%Y-%m-%d")
-        url_data = {
-            "_index": index,
-            "_source": {
-                "timestamp": crawl_time,
-                "url": url,
-                "content": content,
-                "metadata": meta,
-            }
-        }
-        data.append(url_data)
+#         # packing
+#         crawl_time = datetime.now()
+#         index = "index-" + crawl_time.strftime("%Y-%m-%d")
+#         url_data = {
+#             "_index": index,
+#             "_source": {
+#                 "timestamp": crawl_time,
+#                 "url": url,
+#                 "content": content,
+#                 "metadata": meta,
+#             }
+#         }
+#         data.append(url_data)
 
 
 
-    push_crawl_data.delay(data)
-    return str(crawl_time)
-    # with urllib.request.urlopen(url, timeout=timeout) as conn:
-    #     data = conn.read()
-    #     soup = BeautifulSoup(data, 'html.parser')
-    #     metas = soup.find_all('meta')
-    #     return data, metas
+#     push_crawl_data.delay(data)
+#     return str(crawl_time)
+#     # with urllib.request.urlopen(url, timeout=timeout) as conn:
+#     #     data = conn.read()
+#     #     soup = BeautifulSoup(data, 'html.parser')
+#     #     metas = soup.find_all('meta')
+#     #     return data, metas
 
 
 def chunks(lst, n):
@@ -73,17 +73,22 @@ def chunks(lst, n):
 
 def submit_tasks(url_list: List[str]) -> None:
 
-    # for url in url_list:
-    #     crawl.delay(url)
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        future_to_url = {executor.submit(get_content, urls, 60) for urls in chunks(url_list, 50)}
-        for future in concurrent.futures.as_completed(future_to_url):
-            # url = future_to_url[future]
-            try:
-                push_time = future.result()
-                print("[Done] %s" % push_time)
-            except Exception as exc:
-                print("[Exception] %s" % (exc))
+    URL_per_CHUNK = 50
+    for i, urls in enumerate(chunks(url_list, URL_per_CHUNK)):
+        if i > 0 and i % 100 == 0:
+            time.sleep(URL_per_CHUNK - 5)
+        crawl_chain.delay(urls)
+
+
+    # with ThreadPoolExecutor(max_workers=100) as executor:
+    #     future_to_url = {executor.submit(get_content, urls, 60) for urls in chunks(url_list, 50)}
+    #     for future in concurrent.futures.as_completed(future_to_url):
+    #         # url = future_to_url[future]
+    #         try:
+    #             push_time = future.result()
+    #             print("[Done] %s" % push_time)
+    #         except Exception as exc:
+    #             print("[Exception] %s" % (exc))
 
 
 def main():
